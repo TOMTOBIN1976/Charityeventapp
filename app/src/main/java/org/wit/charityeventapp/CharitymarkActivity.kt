41964a0f413320cfff4a4b0ffc58aa33e9fae430
TomTobin <tomtobin0@gmail.com>
2022-10-02 20:46:38 +0100

    Add error logging

--- app/src/main/java/org/wit/charityeventapp/CharitymarkActivity.kt
+++ app/src/main/java/org/wit/charityeventapp/CharitymarkActivity.kt
@@ -2,10 +2,15 @@ package org.wit.charityeventapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class CharitymarkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_charitymark)
+
+        Timber.plant(Timber.DebugTree())
+
+        Timber.i("Charity app Activity started..")
     }
 }
